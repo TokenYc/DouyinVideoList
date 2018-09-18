296d99eaa39e9356766582f2ba1073c4f014bb0d
yc <247067345@qq.com>
2018-09-18 16:12:12 +0800

    修改视频源

--- app/src/main/java/net/archeryc/douyinvideolist/MockData.java
+++ app/src/main/java/net/archeryc/douyinvideolist/MockData.java
@@ -21,8 +21,8 @@ public class MockData {
                 "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v03033ef0000be8v3bsif32ldjudh4c0&line=0", 384, 1138));
         videoList.add(new VideoEntity("https://p1.pstatp.com/large/bd8f000561db440796dd.jpg",
                 "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200fa90000bee4qrqr863tt0o22t50&line=0", 384, 640));
-        videoList.add(new VideoEntity("https://p98.pstatp.com/large/c054000328c6b3983d04.jpg",
-                "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200f500000beff2ht1mikc0n304nog&line=0", 1066, 384));
+        videoList.add(new VideoEntity("https://p98.pstatp.com/large/b34b0007241fa60b9055.jpg",
+                "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200fb90000be5321r2ap9bgas2isc0&line=0", 1066, 384));
         videoList.add(new VideoEntity("https://p98.pstatp.com/large/c0a10007f2fc822bc278.jpg",
                 "https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200f5b0000beflk4tahtm9h94n8nqg&line=0", 360, 640));
         return videoList;
